e4abebe0dc7578a45f9c94944ebe8c0af8ee0459
Neelam Sharma <neelusharma10983@gmail.com>
2024-11-26 10:53:11 -0800

    bug

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
-import WhoWillYouMeet from './components/WhoWillYouMeet';
-import TopicHighlights from './components/TopicHighlights';
-import WhyAttend from './components/WhyAttend';
-import ContactUs from './components/ContactUs';
 import './App.css';
 
 const App = () => (
